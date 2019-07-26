f8c7dd2cb17dfd283949587fcb28754be19d5e95
Gianluca Truda <gianlucatruda@gmail.com>
2019-07-26 18:43:43 +0200

    Added classes for Neurons, Layers, Nets. Added methods for forward prop.

--- main.py
+++ main.py
@@ -10,17 +10,67 @@ def mean_squared_error(truth, pred):
     n = len(truth)
     return 1/n * sum([(y - x)**2 for y,x in zip(truth, pred)])
 
+class Network(ABC):
+
+    def __init__(self, layers=None):
+        if layers is not None:
+            self.layers = layers
+        else:
+            self.layers = []
+
+    def describe(self):
+        for i, layer in enumerate(self.layers):
+            print(f'L{i}:\t{layer.describe()}')
+
+    def feed_forward(self, inputs):
+        # Forward propagate inputs through each layer in net
+        outs = inputs
+        for layer in self.layers:
+            outs = layer.forward(outs)
+            print(len(outs))
+        return outs
+
+
+class Layer(ABC):
+
+    def __init__(self, neurons=None):
+        if neurons is not None:
+            self.neurons = neurons
+        else:
+            self.neurons = []
+    
+    def describe(self):
+        desc = ''
+        for i, neuron in enumerate(self.neurons):
+            desc += f'N{i}({neuron.size}) '
+        return desc
+
+    def forward(self, inputs):
+        # Feed forward inputs through the layer
+        outputs = []
+        for neuron in self.neurons:
+            outputs.append(neuron.activate(inputs))
+        return outputs
+
+
 class Neuron(ABC):
    
-    def __init__(self):
-        activation = None
-        input_function = None
-        output_function = None
+    def __init__(self, size, act_func=relu):
+        # Size is the number of neurons in previous layer / inputs
+        self.__weights = [1.0 for i in range(size)]
+        self.__bias = 0
+        self.__act_func = act_func
 
+    @property
+    def size(self):
+        return len(self.__weights)
 
-class Connection(ABC):
+    def activate(self, inputs):
+        # Activate neuron with given inputs
+        assert(self.size) == len(inputs))
+        activation = self.__bias
+        activation += sum([self.__weights[i] * inputs[i] 
+            for i, _ in enumerate(inputs)]) 
+        return self.__act_func(activation)
 
-    def __init__(self):
-        weight = None
-        bias = None
-
+    
