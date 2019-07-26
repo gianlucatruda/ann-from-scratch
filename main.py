64bd913a95c76c40c0eadebb58c6c59f924d3166
Gianluca Truda <gianlucatruda@gmail.com>
2019-07-26 16:28:40 +0200

    Added basic ReLU and MSE functions

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from abc import ABC
 
+def relu(x):
+    # Simple ReLU function
+    return max(0, x)
+
+def mean_squared_error(truth, pred):
+    # MSE function
+    assert(len(truth) == len(pred))
+    n = len(truth)
+    return 1/n * sum([(y - x)**2 for y,x in zip(truth, pred)])
+
 class Neuron(ABC):
    
     def __init__(self):
